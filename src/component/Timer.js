fe9369e271e44702ed336b8c1d8636e5cac01b3c
huan-chin chen <cherry032826@gmail.com>
2024-01-25 00:04:32 +0800

    first commit

---
+++ src/component/Timer.js
@@ -0,0 +1,25 @@
+import { useEffect } from "react";
+
+function Timer({ secondsRemaining, dispatch }) {
+  const min = Math.floor(secondsRemaining / 60);
+  const seconds = secondsRemaining % 60;
+
+  useEffect(
+    function () {
+      const id = setInterval(function () {
+        dispatch({ type: "tick" });
+      }, 1000);
+      return () => clearInterval(id);
+    },
+    [dispatch]
+  );
+  return (
+    <div className="timer">
+      {min < 10 ? "0" : ""}
+      {min}:{seconds < 10 ? "0" : ""}
+      {seconds}
+    </div>
+  );
+}
+
+export default Timer;
